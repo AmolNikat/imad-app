029030b759d5fd0175be2dd6ff30902f6341c3f8
AmolNikat <amolnikat13@gmail.com>
2017-08-06 11:26:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 
 app.get('/article-one',function(req,res){
     
-    res.send(createtemplete(articelOne));
+    res.send(createTemplet(articelOne));
     
     
 });
